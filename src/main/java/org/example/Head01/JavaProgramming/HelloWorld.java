21c15a8cbbc389e3a3cabbce10b09a8a38623e77
윤금비 <gkrry0404@naver.com>
2025-12-31 18:46:28 +0900

    ì오늘 짱이야

--- src/main/java/org/example/Head01/JavaProgramming/HelloWorld.java
+++ src/main/java/org/example/Head01/JavaProgramming/HelloWorld.java
@@ -1,5 +1,12 @@
+package org.example.Head01.JavaProgramming;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
+        int a = 10;
+        int b = a;  // b는 10을 복사해서 가진다.
+
+        b = 20;
+        System.out.println(a); // 출력: 10 (영향 없음)
+    }
 }
+
