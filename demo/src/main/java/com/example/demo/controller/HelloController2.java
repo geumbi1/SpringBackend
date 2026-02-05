e80a73935e7c1f5dce442471aca3acc0ef798600
geumbi <gkrry0404@naver.com>
2026-02-05 17:49:31 +0900

    first-commit

---
+++ demo/src/main/java/com/example/demo/controller/HelloController2.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class HelloController2 {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello, Spring MVC!";
+    }
+}
